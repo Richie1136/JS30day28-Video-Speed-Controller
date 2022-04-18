3f569eb879b99ccf11165a19bf4b60fd7ed0e2ea
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 14:42:22 -0400

    Finished JS30day28project

--- main.js
+++ main.js
@@ -3,16 +3,12 @@ const bar = document.querySelector('.speed-bar')
 const video = document.querySelector('.flex')
 
 function GetPercent(e) {
-  console.log(e)
   const y = e.pageY - this.offsetTop;
-  console.log(y)
   const percent = y / this.offsetHeight
   const min = 0.4
   const max = 4
   const height = Math.round(percent * 100) + "%"
   const playbackRate = percent * (max - min) + min
-  console.log(height)
-  console.log(playbackRate)
   bar.style.height = height
   bar.textContent = `${playbackRate.toFixed(2)}x`
   video.playbackRate = playbackRate
