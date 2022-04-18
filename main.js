9677fd053c93650da1e0fb3c9f04ee903de47857
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 14:24:42 -0400

    Using Math.round to round the number and adding the percent sign to the end of the number

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ function GetPercent(e) {
   console.log(e)
   const y = e.pageY - this.offsetTop;
   console.log(y)
-  const percent = y / this.offsetHeight * 100
-  console.log(percent)
+  const percent = y / this.offsetHeight
+  const min = 0.4
+  const max = 4
+  const height = Math.round(percent * 100) + "%"
+  console.log(height)
 }
 
 speed.addEventListener('mousemove', (GetPercent))
