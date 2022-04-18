7ce3dccb21a72a59f7c5f60be03882ceb3a9ffbf
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-18 14:14:57 -0400

    Forgot to add my js file to my index.html

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const speed = document.querySelector('.speed')
 const bar = document.querySelector('.speed-bar')
 const video = document.querySelector('.flex')
+
+speed.addEventListener('mousemove', (e) => {
+  console.log(e)
+})
